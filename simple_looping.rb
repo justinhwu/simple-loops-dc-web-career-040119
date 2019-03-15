772fbda3296236b6e595065d450d4d1878305605
Justin Wu <justinhwu95@gmail.com>
2019-03-15 14:59:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -49,6 +49,9 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  for counter in 
+  for counter in 1..number_of_times do
+    puts phrase
+    counter+=1
+  end
 end
 
