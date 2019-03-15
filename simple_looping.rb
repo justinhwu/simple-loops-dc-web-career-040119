aa53b349de3039ed7961a1aeb7c241ffc4568e72
Justin Wu <justinhwu95@gmail.com>
2019-03-15 15:01:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,8 +14,7 @@ def loop_iterator(number_of_times)
   if counter < number_of_times
   phrase = "Welcome to Flatiron School's Web Development Course!"
   puts phrase
-  counter = 0
-  counter++
+
 end
 end
 end
